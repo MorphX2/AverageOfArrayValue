dc4257091a78bfc921e64a1e041f8eb7c8d50b71
Jamaal Crichfield <jcrichfield2@gmail.com>
2017-12-02 15:18:34 -0500

    Wrote a program to calculate the average from summing a series of integers

---
+++ src/AverageOfArrayValue.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author jamaalcrichfield
+ */
+public class AverageOfArrayValue {
+    public static void main(String[] args) {
+        int[] integerArray = { 34, 22, 56, 77, 23, 79, 44, 89 };
+        int sum = 0;
+        int elementCounter = 0;
+        int average = 0;
+        
+        // Print all the array elements
+        for (int index = 0; index < integerArray.length; index++) {
+            System.out.println(integerArray[index]);
+            sum = sum + integerArray[index];
+            
+            elementCounter = elementCounter + 1;
+        }
+        average = sum / elementCounter;
+        System.out.println("The average of all " + elementCounter +  
+                " elements in the array is: " + average );
+      
+        
+    }
+    
+}
